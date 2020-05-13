5d3238b2851c07a5f3d564751ba55f6899945d03
Alexander Nikolaev <ni-ko-la@mail.ru>
2020-05-13 23:06:01 +0300

    ffi_lib_flags

--- sq.rb
+++ sq.rb
@@ -1,6 +1,7 @@
 require 'ffi'
 module Sq
   extend FFI::Library
+  ffi_lib_flags :lazy, :global
   ffi_lib File.expand_path("./libsq.so", File.dirname(__FILE__))
   attach_function :sq, [:int], :int
 end
